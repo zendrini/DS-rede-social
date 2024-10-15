72c1e9712ebe1b2e68498a3324c6f2369a0f8abf
Richardson Schawarski Cruz <125823124+professorrichardson@users.noreply.github.com>
2024-10-15 19:58:20 -0300

    Aula 09 end

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,33 +1,33 @@
 import { getCSS } from "./common.js";
 
 async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
-    const res = await fetch(url)
-    const dados = await res.json()
-    const nomeDasRedes = Object.keys(dados)
-    const quantidadeUsuarios = Object.values(dados)
-  
-    const data = [
-        {
-          x: nomeDasRedes,
-          y: quantidadeUsuarios,
-          type: 'bar',
-          marker: {
-            color: getCSS('--primary-color')
-          }
-        }
-      ]
-  
-      const layout = {
-        plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bg-color')
+  const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+  const res = await fetch(url)
+  const dados = await res.json()
+  const nomeDasRedes = Object.keys(dados)
+  const quantidadeUsuarios = Object.values(dados)
+
+  const data = [
+    {
+      x: nomeDasRedes,
+      y: quantidadeUsuarios,
+      type: 'bar',
+      marker: {
+        color: getCSS('--primary-color')
+      }
     }
+  ]
+
+  const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color')
+  }
 
   const grafico = document.createElement('div')
   grafico.className = 'grafico'
   document.getElementById('graficos-container').appendChild(grafico)
   Plotly.newPlot(grafico, data, layout)
-  
-  }
-  
-  quantidadeUsuarios()
+
+}
+
+quantidadeUsuarios()
